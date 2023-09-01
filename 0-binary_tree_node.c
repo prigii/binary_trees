dcb35e46f7142201fe87451711cc705c6ff1c0c2
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 12:53:04 +0300

    corrected

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  * Return: Pointer to newly created node or NULL on failure
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
