60d27aaedc7b22b30f86fd2713c3286f33819163
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:55:21 +0300

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,17 +6,12 @@
  * Return: pointer to sibling node, NULL if node, parent are NULL or no sibling
  */
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL) {
-        return NULL; // If node is NULL or has no parent, return NULL
-    }
-
-    // Check if the node is the left child of its parent
-    if (node->parent->left == node) {
-        return node->parent->right; // Return the right child of the parent
-    }
-
-    // If the node is the right child of its parent or not a child at all
-    return node->parent->left; // Return the left child of the parent
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
