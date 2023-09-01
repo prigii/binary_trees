b7e198b2b8fe8e0e4966fc949e19856b4128e586
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:14:10 +0300

    betty adjusted

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,11 +6,7 @@
  * Return: 1 if node is root, 0 if not root or is NULL
  */
 
-int binary_tree_is_root(const binary_tree_t *node) {
-    if (node == NULL) {
-        return 0; // If node is NULL, not a root
-    }
-
-    // A root node is one that has no parent (parent is NULL)
-    return (node->parent == NULL);
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return ((!node || node->parent) ? 0 : 1);
 }
