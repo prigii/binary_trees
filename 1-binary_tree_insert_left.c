2e7362002af24cf6bd9795b5463bc6a2da0f56ac
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:04:54 +0300

    corrected

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,29 +7,23 @@
  * Return: Pointer to newly created node NULL on failure NULL if parent is NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
-    if (parent == NULL) {
-        return NULL; // Cannot insert as left child if parent is NULL
-    }
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL; // Allocation failure
-    }
+	if (!parent)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
 
-    new_node->value = value;
-    new_node->parent = parent;
-    new_node->left = parent->left; // Move the current left child to the new node's left child
-    new_node->right = NULL;
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = parent->left;
+	parent->left = new;
 
-    if (parent->left != NULL) {
-        parent->left->parent = new_node; // Update the parent of the current left child
-    }
-
-    parent->left = new_node; // Set the new node as the left child of the parent
-
-    return new_node;
+	if (new->left)
+		new->left->parent = new;
+	return (new);
 }
-
-
-
