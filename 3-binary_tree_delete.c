33591ef1073ecc68d4379e067afdaf96581cb37a
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:09:25 +0300

    reviewed

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,19 +5,11 @@
  * @tree: the tree to be deleted
  */
 
-
-void binary_tree_delete(binary_tree_t *tree) {
-    if (tree == NULL) {
-        return; // If tree is NULL, do nothing
-    }
-
-    // Recursively delete left and right subtrees
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-
-    // Once subtrees are deleted, free the current node
-    free(tree);
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
-
-
-
