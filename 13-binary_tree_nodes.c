64c358b0f960bdd39f91ba84c2a0453594e5fc1b
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:41:43 +0300

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,16 +6,38 @@
  * Return: number of nodes counted, 0 if tree is NULL
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0; // If tree is NULL, there are no nodes
-    }
-
-    if (tree->left != NULL || tree->right != NULL) {
-        // If node has at least one child, count this node
-        return 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-    } else {
-        return 0; // If node is a leaf (no children), don't count it
-    }
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
 
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to be measured
+ * Return: size of tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree whose leaves are to be counted
+ * Return: number of leaves, 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
