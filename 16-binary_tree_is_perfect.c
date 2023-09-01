d7ccb974836b5e5fdd82bc9debfe3a7e1d26b381
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:52:45 +0300

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,22 +1,64 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to root node of tree to be checked
+ * Return: 1 if tree is perfect, 0 if not perfect or is NULL
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->right && !tree->left)
+		return (1);
+
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
+	power = (size_t)_pow_recursion(2, height + 1);
+
+	return (power - 1 == nodes);
+}
+
+/**
+ * _pow_recursion - returns value of x raised to power of y
+ * @x: value to be raised
+ * @y: the value of power
+ * Return: x to power of y, or -1 if y is negative
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: the tree whose height is to be measured
  * Return: height of tree, 0 if tree is NULL
  */
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0; // If tree is NULL, height is 0
-    }
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-    // Recursively calculate the height of the left and right subtrees
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    // Return the maximum height between left and right subtrees plus 1 for the current node
-    return (left_height > right_height) ? left_height + 1 : right_height + 1;
+	return (height_l > height_r ? height_l : height_r);
 }
 
 /**
@@ -25,40 +67,9 @@ size_t binary_tree_height(const binary_tree_t *tree) {
  * Return: size of tree, 0 if tree is NULL
  */
 
-size_t binary_tree_size(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0; // If tree is NULL, size is 0
-    }
-
-    // Recursively calculate the size of the left and right subtrees
-    size_t left_size = binary_tree_size(tree->left);
-    size_t right_size = binary_tree_size(tree->right);
-
-    // Return the sum of sizes of the left and right subtrees plus 1 for the current node
-    return left_size + right_size + 1;
-}
-
-/**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to root node of tree to be checked
- * Return: 1 if tree is perfect, 0 if not perfect or is NULL
- */
-
-
-int binary_tree_is_perfect(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0; // If tree is NULL, not a perfect tree
-    }
-
-    // Calculate the height of the left and right subtrees
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
-
-    // Check if the tree is balanced and the left and right subtrees have the same height
-    if (left_height == right_height) {
-        // Check if the left and right subtrees are also perfect
-        return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
-    }
-
-    return 0;
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
