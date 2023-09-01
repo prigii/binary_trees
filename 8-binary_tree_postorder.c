e432283eb7148684bd66115d6f87e4ab4d1d24b4
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:21:27 +0300

    reviewed

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,17 +6,11 @@
  * @func: pointer to function to be called for each node
  */
 
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)) {
-    if (tree == NULL || func == NULL) {
-        return; // If tree or func is NULL, do nothing
-    }
-
-    // Traverse the left subtree in post-order
-    binary_tree_postorder(tree->left, func);
-
-    // Traverse the right subtree in post-order
-    binary_tree_postorder(tree->right, func);
-
-    // Call the function for the current node
-    func(tree->value);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
