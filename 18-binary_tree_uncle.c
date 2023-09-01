02fdef491ffaf0545f12b70d358f6aaa2fac589d
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 13:58:05 +0300

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,12 +6,11 @@
  * Return: pointer to uncle of node or NULL
  */
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL) {
-        return NULL; // If node is NULL or has no parent, return NULL
-    }
-
-    return binary_tree_sibling(node->parent); // Return the sibling of the parent
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
 
 /**
@@ -20,16 +19,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
  * Return: pointer to sibling node, NULL if node, parent are NULL or no sibling
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL) {
-        return NULL; // If node is NULL or has no parent, return NULL
-    }
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
 
-    // Check if the node is the left child of its parent
-    if (node->parent->left == node) {
-        return node->parent->right; // Return the right child of the parent
-    }
-
-    // If the node is the right child of its parent or not a child at all
-    return node->parent->left; // Return the left child of the parent
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
