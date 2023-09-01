0a31290eb7863b4af80cf7fde2293d2cb55d9b2b
Peter Rigii <theonlyrigii@gmail.com>
2023-09-01 14:09:52 +0300

    levelorder

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,68 +6,52 @@
  * @func: pointer to function to be called for each node
  */
 
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int)) {
-    if (tree == NULL || func == NULL) {
-        return;
-    }
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t level, maxlevel;
 
-    Queue *queue = createQueue();
-    enqueue(queue, tree);
+	if (!tree || !func)
+		return;
 
-    while (!isQueueEmpty(queue)) {
-        const binary_tree_t *current = dequeue(queue);
-        func(current->value);
+	maxlevel = binary_tree_height(tree) + 1;
 
-        if (current->left != NULL) {
-            enqueue(queue, current->left);
-        }
-        if (current->right != NULL) {
-            enqueue(queue, current->right);
-        }
-    }
-
-    freeQueue(queue);
+	for (level = 1; level <= maxlevel; level++)
+		lo_helper(tree, func, level);
 }
 
-Queue *createQueue() {
-    Queue *queue = malloc(sizeof(Queue));
-    queue->front = queue->rear = NULL;
-    return queue;
+/**
+ * lo_helper - uses post-order traversal to go through a binary tree
+ * @tree: tree to be traversed
+ * @func: pointer to function to be called for each node
+ * @level: level of tree to call func upon
+ */
+
+void lo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		lo_helper(tree->left, func, level - 1);
+		lo_helper(tree->right, func, level - 1);
+	}
 }
 
-void enqueue(Queue *queue, const binary_tree_t *node) {
-    QueueNode *newNode = malloc(sizeof(QueueNode));
-    newNode->node = node;
-    newNode->next = NULL;
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree whose height is to be measured
+ * Return: height of tree, 0 if tree is NULL
+ */
 
-    if (queue->rear == NULL) {
-        queue->front = queue->rear = newNode;
-        return;
-    }
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-    queue->rear->next = newNode;
-    queue->rear = newNode;
+	if (!tree)
+		return (0);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (height_l > height_r ? height_l : height_r);
 }
-
-const binary_tree_t *dequeue(Queue *queue) {
-    if (queue->front == NULL) {
-        return NULL;
-    }
-
-    QueueNode *temp = queue->front;
-    queue->front = queue->front->next;
-
-    if (queue->front == NULL) {
-        queue->rear = NULL;
-    }
-
-    const binary_tree_t *node = temp->node;
-    free(temp);
-    return node;
-}
-
-int isQueueEmpty(Queue *queue) {
-    return (queue->front == NULL);
-}
-
-
